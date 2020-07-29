8e83a3109c98a9e9759c44bbdd6abc2f35a81db7
unknown <james.casas@vallegrande.edu.pe>
2020-07-28 20:06:42 -0500

    nada

--- api/src/main/java/com/jquispeluyo/azureml/services/AzureMl.java
+++
@@ -1,9 +0,0 @@
-package com.jquispeluyo.azureml.services;
-
-import com.jquispeluyo.azureml.models.input.dto.PayloadDto;
-import com.jquispeluyo.azureml.models.output.dto.PayloadOutDto;
-
-public interface AzureMl {
-    PayloadOutDto predict (PayloadDto obj);
-    PayloadOutDto predictOnlyResult (PayloadDto obj);
-}
